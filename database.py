8dc3315a359daf643b7d73f38c894e486d39f68e
Bartosz Skłodowski <sklodowski94@gmail.com>
2022-06-04 11:35:31 +0200

    Change SQLALCHEMY_DATABASE_URL.

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:DaftAcademy@127.0.0.1:5555"
-SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://xsjljnrcbrrfdu:19e5ddeb78a4dbb7c9d077cca13d69ea106c0b6832f552d1b5dd10ca47b99322@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/dab1htb6r28ekk")
+SQLALCHEMY_DATABASE_URL = "postgresql://xsjljnrcbrrfdu:19e5ddeb78a4dbb7c9d077cca13d69ea106c0b6832f552d1b5dd10ca47b99322@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/dab1htb6r28ekk"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
